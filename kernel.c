d545df238af975f8d4ad6038e9282349f474e362
Tomohiro Matsuo <gacktomo@gmail.com>
2023-09-27 14:42:18 +0000

    Add panic

--- kernel.c
+++ kernel.c
@@ -30,6 +30,7 @@ void putchar(char ch)
 
 void kernel_main(void)
 {
+	memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 	printf("\n\nHello %s\n", "World!");
 	printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
