cd0a2ddc617c1ff118a8e517053761b29bbd1fca
Tomohiro Matsuo <gacktomo@gmail.com>
2023-09-27 16:25:17 +0000

    Add context switch

--- kernel.h
+++ kernel.h
@@ -17,6 +17,18 @@ struct sbiret
 		}                                                                     \
 	} while (0)
 
+#define PROCS_MAX 8			// 最大プロセス数
+#define PROC_UNUSED 0		// 未使用のプロセス管理構造体
+#define PROC_RUNNABLE 1 // 実行可能なプロセス
+
+struct process
+{
+	int pid;						 // プロセスID
+	int state;					 // プロセスの状態
+	vaddr_t sp;					 // コンテキストスイッチ時のスタックポインタ
+	uint8_t stack[8192]; // カーネルスタック
+};
+
 struct trap_frame
 {
 	uint32_t ra;
